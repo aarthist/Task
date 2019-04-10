7b9bff29c54db169e4e10a1879d59c0acc869cd8
aarthist <49455751+aarthist@users.noreply.github.com>
2019-04-10 17:27:00 +0530

    commit with mysql database conecion

--- backend/config/datastore.js
+++ backend/config/datastore.js
@@ -6,6 +6,7 @@ const component = "DATASTORE";
 const mongoose = require('mongoose');
 const config = require('config');
 const url = require('url');
+const mysql = require('mysql2');
 
 
 mongoose.Promise = global.Promise;
@@ -27,12 +28,18 @@ mongoose.connection.on('error', (err)=> {
 });
 
 function connect() {
-    const log = require('../util/logger').log(component, __filename);
-    var MONGO_URI = `mongodb://${config.mongo.host}:${config.mongo.port}/${config.mongo.database}`;
-    log.debug(component, `connecting to: ${config.mongo.host}:${config.mongo.port}/${config.mongo.database}`);
-    var mongoURI = url.parse(MONGO_URI);
-    mongoose.connect(mongoURI.href, { useMongoClient: true });
-    log.close();
+    if(config.databaseConnetion == "MongoDB") {
+        var MONGO_URI = `mongodb://${config.mongo.host}:${config.mongo.port}/${config.mongo.database}`;
+        var mongoURI = url.parse(MONGO_URI);
+        mongoose.connect(mongoURI.href, { useMongoClient: true });
+    } else {
+        var SQL_URL = mysql.createConnection(config.sql);
+        console.log(SQL_URL)
+        SQL_URL.connect(function(err) {
+            console.log(err);
+        })
+    }
+
 
 }
 
